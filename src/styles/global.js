b09a19f6488b3ad13a057ce15a0a15cf64cbdb20
bBraian <braianvoficial@gmail.com>
2023-09-03 19:17:25 -0300

    initializing

---
+++ src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${props => props.theme['primary-50']};
+    }
+
+    body {
+        background-color: ${props => props.theme['white-0']};
+        color: ${props => props.theme['neutral-90']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input-security, textarea, button {
+        font: 400 1rem Poppins, sans-serif;
+    }
+`;
---
+++ src/styles/themes/default.js
@@ -0,0 +1,33 @@
+export const defaultTheme = {
+    'white-0': '#fff',
+
+    'neutral-10': '#F1F1F1',
+    'neutral-20': '#D9D9D9',
+    'neutral-30': '#C1C1C1',
+    'neutral-40': '#A9A9A9',
+    'neutral-50': '#919191',
+    'neutral-60': '#797979',
+    'neutral-70': '#606060',
+    'neutral-80': '#484848',
+    'neutral-90': '#303030',
+    'neutral-100': '#181818',
+
+    'primary-10': '#F9D8D8',
+    'primary-20': '#F3B2B2',
+    'primary-30': '#EE8B8B',
+    'primary-40': '#E86565',
+    'primary-50': '#E23E3E',
+    'primary-60': '#CB3838',
+    'primary-70': '#B53232',
+    'primary-80': '#9E2B2B',
+    'primary-90': '#882525',
+    'primary-100': '#711F1F',
+
+    'rating': '#FFB660',
+
+    'success-100': '#31B057',
+    'success-10': '#CEECD7',
+
+    'error-10': '#FDE7EB',
+    'error-100': '#EE1133'
+}
